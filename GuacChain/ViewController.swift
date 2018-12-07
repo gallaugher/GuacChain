56ae39e14d6a7b186774c3bf6f7cc0ca4d75f655
John Gallaugher <john.gallaugher@bc.edu>
2018-12-07 10:53:45 -0500

    Initial Commit

---
+++ GuacChain/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GuacChain
+//
+//  Created by John Gallaugher on 12/7/18.
+//  Copyright © 2018 John Gallaugher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
